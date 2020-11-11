845331472d6c55eb3d56645cd58c14166c86d538
Samed Alhajajla <41950927+samedhaa@users.noreply.github.com>
2020-11-11 18:19:30 +0200

    Create B - Lotto UVA - 441.cpp

---
+++ B - Lotto UVA - 441.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<int>ans,v;
+void comb(int i) {
+    if(ans.size() == 6) {
+        cout<<ans[0];
+        for(int j=1;j<6;j++)cout<<" "<<ans[j];
+        cout<<endl;
+        return;
+    }
+    for(int j=i;j<v.size();j++) {
+        ans.push_back(v[j]);
+        comb(j+1);
+        ans.pop_back();
+    }
+}
+
+int main()
+{
+    int a;
+    bool f = false;
+    while(cin>>n && n) {
+        if(f)cout<<endl;
+        f=true;
+        v.clear();
+        for(int i=0;i<n;i++) {
+            cin>>a;
+            v.push_back(a);
+        }
+        comb(0);
+    }
+    return 0;
+}
